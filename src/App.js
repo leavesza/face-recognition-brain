dbbe381e12524246b3efb12e1941909a38cf7a55
leavesza <yaseenlevy21@gmail.com>
2023-08-15 23:31:35 +0200

    port change

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ class App extends Component {
     .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://52.19.137.202:3000/image', {
+          fetch('https://52.19.137.202:443/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
