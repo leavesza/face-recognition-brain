5169a61cb49f9208bb9a52b6bd82af102ba6a74a
leavesza <yaseenlevy21@gmail.com>
2023-08-15 12:28:11 +0200

    using https

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component{
     }
 
     onSubmitSignIn = () =>{
-        fetch('http://52.19.137.202:3000/signin', { 
+        fetch('https://52.19.137.202:3000/signin', { 
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
