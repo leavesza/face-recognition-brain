dbf67540b6c00bd01b140d121031075f074cd247
leavesza <yaseenlevy21@gmail.com>
2023-08-15 12:03:10 +0200

    updated endpoints

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ class App extends Component {
     .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://ec2-52-19-137-202.eu-west-1.compute.amazonaws.com:3000/image', {
+          fetch('http://52.19.137.202:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
